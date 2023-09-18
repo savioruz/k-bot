b802c18bd7c37ecd9e1ddea58928b0b9e76de576
jakues <jakueenak@gmail.com>
2023-09-18 16:16:58 +0700

    Update translation using libretranslate

--- App/translate.js
+++ App/translate.js
@@ -3,27 +3,22 @@ class Translate {
     this.chat = chat;
   }
 
-  async doTranslate(apiKey, queries, source = "en", target = "id") {
-    const encodedParams = new URLSearchParams();
-    encodedParams.set("source", source);
-    encodedParams.set("target", target);
-    encodedParams.set("q", queries);
-
-    const options = {
+  async doTranslate(apiKey, queries, source = "auto", target = "id") {
+    const req = await fetch("http://freeze.masmanto.xyz:5000/translate", {
       method: "POST",
-      url: "https://google-translate1.p.rapidapi.com/language/translate/v2",
-      headers: {
-        "content-type": "application/x-www-form-urlencoded",
-        "Accept-Encoding": "application/gzip",
-        "X-RapidAPI-Key": apiKey,
-        "X-RapidAPI-Host": "google-translate1.p.rapidapi.com",
-      },
-      data: encodedParams,
-    };
+      body: JSON.stringify({
+        q: queries,
+        source: source,
+        target: target,
+        format: "text",
+        api_key: apiKey
+      }),
+      headers: { "Content-Type": "application/json" },
+    });
 
     try {
-      const response = await axios.request(options);
-      return response.data;
+      const response = req.json();
+      return response.translatedText;
     } catch (err) {
       console.log("[error] Failed to fetch url translation: ", err);
     }
@@ -35,16 +30,16 @@ class Translate {
 
       if (msg.body === COMMAND_TR) {
         if (msg.hasQuotedMsg) {
-            const quotedMsg = await msg.getQuotedMessage();
-            if (quotedMsg.body) {
-                text = quotedMsg.body;
-            }
-            const request = await this.doTranslate(key, text);
-            const result = request.data.translations[0].translatedText;
+          const quotedMsg = await msg.getQuotedMessage();
+          if (quotedMsg.body) {
+            text = quotedMsg.body;
+          }
+          const request = await this.doTranslate(key, text);
+          const result = request.data.translations[0].translatedText;
 
-            await this.chat.sendReply(msg, `Translate result:\n${result}`);
+          await this.chat.sendReply(msg, `Translate result:\n${result}`);
         } else {
-            await this.chat.sendReply(msg, "Quote chat to translate !");
+          await this.chat.sendReply(msg, "Quote chat to translate !");
         }
       }
     } catch (err) {
@@ -54,9 +49,8 @@ class Translate {
 }
 
 const Chat = require("../vendor/chat");
-const axios = require("axios");
 
-const key = "d5d4e9bb7cmshfb961a1e2c72fd1p13586fjsn984a527fb6cb";
+const key = "8aaa6fad-8c2b-4447-9d52-e0d93204a2b7";
 const COMMAND_TR = "/tr";
 
 const chat = new Chat();
